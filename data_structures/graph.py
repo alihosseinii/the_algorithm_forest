a30858fbc6ada9ac56e6c5897188aab8e5ebfaf0
seyed ali hosseini <alihosseinihf63@gmail.com>
2026-08-02 09:11:53 +0330

    classes and functions added to graph and usermanager

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -1,2 +1,43 @@
 class Graph:
-    pass
+    def __init__(self):
+        # node -> list[(neighbor, weight)]
+        self._adjacency = {}
+
+    def add_node(self, node):
+        if node not in self._adjacency:
+            self._adjacency[node] = []
+
+    def add_edge(self, u, v, weight):
+        self.add_node(u)
+        self.add_node(v)
+        if not self.has_edge(u, v):
+            self._adjacency[u].append((v, weight))
+            self._adjacency[v].append((u, weight))
+
+    def neighbors(self, node):
+        """Returns list of (neighbor, weight) tuples."""
+        return self._adjacency.get(node, [])
+
+    def neighbor_names(self, node):
+        return [n for n, _w in self.neighbors(node)]
+
+    def nodes(self):
+        return list(self._adjacency.keys())
+
+    def has_node(self, node):
+        return node in self._adjacency
+
+    def has_edge(self, u, v):
+        return any(n == v for n, _w in self._adjacency.get(u, []))
+
+    def edge_weight(self, u, v):
+        for n, w in self._adjacency.get(u, []):
+            if n == v:
+                return w
+        return None
+
+    def __len__(self):
+        return len(self._adjacency)
+
+    def __repr__(self):
+        return f"Graph(nodes={len(self)})"
--- users/user_manager.py
+++ users/user_manager.py
@@ -2,5 +2,46 @@ class User:
     def __init__(self, username, password_hash, score=0):
         pass
 
+    def to_dict(self):
+        return {
+            "username": self.username,
+            "password_hash": self.password_hash,
+            "score": self.score,
+        }
+
+    @staticmethod
+    def from_dict(d):
+        return User(d["username"], d["password_hash"], d.get("score", 0))
+
+
 class UserManager:
-    pass
+    def __init__(self):
+           pass
+
+    def _load(self):
+        pass
+
+    def _save(self):
+        pass
+
+    def _rebuild_indexes(self):
+        pass
+
+    def username_exists(self):
+        pass
+
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+    def add_score(self, username, delta_score):
+        pass
+
+    def get_score(self, username):
+        pass
+
+    def top_players(self, k=10):
+        pass
+        
