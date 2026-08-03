44fdbfbfd88a2677e3f28514b992caee12dbd39c
seyed ali hosseini <alihosseinihf63@gmail.com>
2026-08-03 12:09:13 +0330

    data_structures completed

--- data_structures/max_heap.py
+++ data_structures/max_heap.py
@@ -1,2 +1,61 @@
 class MaxHeap:
-    pass
+    def __init__(self):
+        self._heap = []
+    def _sift_up(self, i):
+        while i > 0:
+            parent = (i - 1) // 2
+            if self._heap[i][0] > self._heap[parent][0]:
+                self._heap[i], self._heap[parent] = self._heap[parent], self._heap[i]
+                i = parent
+            else:
+                break
+
+    def _sift_down(self, i):
+        n = len(self._heap)
+        while True:
+            left, right = 2 * i + 1, 2 * i + 2
+            largest = i
+            if left < n and self._heap[left][0] > self._heap[largest][0]:
+                largest = left
+            if right < n and self._heap[right][0] > self._heap[largest][0]:
+                largest = right
+            if largest == i:
+                break
+            self._heap[i], self._heap[largest] = self._heap[largest], self._heap[i]
+            i = largest
+
+    def push(self, score, username):
+        self._heap.append((score, username))
+        self._sift_up(len(self._heap) - 1)
+
+    def pop_max(self):
+        if not self._heap:
+            return None
+        top = self._heap[0]
+        last = self._heap.pop()
+        if self._heap:
+            self._heap[0] = last
+            self._sift_down(0)
+        return top
+
+    def peek_max(self):
+        return self._heap[0] if self._heap else None
+
+    def top_k(self, k):
+        temp = MaxHeap()
+        temp._heap = list(self._heap)
+        result = []
+        for _ in range(min(k, len(temp._heap))):
+            top = temp.pop_max()
+            if top is None:
+                break
+            result.append(top)
+        return result
+
+    def rebuild(self, score_username_pairs):
+        self._heap = []
+        for score, username in score_username_pairs:
+            self.push(score, username)
+
+    def __len__(self):
+        return len(self._heap)
--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,2 +1,46 @@
+class _Node:
+    __slots__ = ("data", "next")
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
 class Stack:
-    pass
+    def __init__(self):
+        self._top = None
+        self._size = 0
+
+    def push(self, item):
+        self._top = _Node(item, self._top)
+        self._size += 1
+
+    def pop(self):
+        if self._top is None:
+            raise IndexError("pop from an empty stack")
+        node = self._top
+        self._top = node.next
+        self._size -= 1
+        return node.data
+
+    def peek(self):
+        if self._top is None:
+            raise IndexError("peek from an empty stack")
+        return self._top.data
+
+    def is_empty(self):
+        return self._top is None
+
+    def clear(self):
+        self._top = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def __repr__(self):
+        items = []
+        node = self._top
+        while node:
+            items.append(node.data)
+            node = node.next
